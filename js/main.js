1111fe6675393e40c174223b4d37b9b0e115b00f
Gut_PC <raulnlh@gmail.com>
2022-11-17 17:09:35 +0100

    pasar strings de javascript a py-script

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 
 var strCode = "Prueba";
 
-function getStringCode(){
+function getStringCodeFromHTML(){
     strCode = document.getElementById("codeTextarea1").value;
     console.log(strCode);
 }
 
+/* function pruebaDeGlobales(){
+    console.log(strCode);
+} */
+
+function getstrCode(){
+    return strCode;
+}
+
+const element = document.getElementById("buttonRun");
+element.addEventListener("click", function(){
+    getStringCodeFromHTML()
+    //pruebaDeGlobales()
+ });
 
 console.log(strCode);
+
+//alert("Prueba desde js")
---
+++ python/controlador.py
@@ -0,0 +1,17 @@
+#import asyncio
+from pyodide.ffi import create_proxy        #Go to inspect -> settings gear -> Uncheck 'enable javascript source maps' and 'enable css source map'. https://github.com/dart-lang/webdev/issues/1500
+from js import alert, document, getstrCode
+
+
+def button_click(event):                    #crear antes de create_proxy
+ alert(f"Prueba desde {getstrCode()}")
+
+
+click_proxy = create_proxy(button_click)
+#document.getElementById("buttonRun").addEventListener("click", click_proxy)
+e = document.getElementById("buttonRun")
+e.addEventListener("click", click_proxy)
+
+
+
+
