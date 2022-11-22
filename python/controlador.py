bbe15dc30987371cfcc3238dfd3319a4373e53a2
Gut_PC <raulnlh@gmail.com>
2022-11-22 13:26:28 +0100

    Arreglos

--- python/controlador.py
+++ python/controlador.py
@@ -11,23 +11,23 @@ def execCodigo():
     stringCode = getstrCode()
     stringCodeMod='global x\n'+stringCode   #pyodide necesita declarar global la variable que usamos?
     exec(stringCodeMod)
-    resultCode = x                          #sacamos la variable que queremos del codigo pasado a exec
+    resultCode = x                          #variable a sacar del codigo pasado a exec
     print('x',resultCode)
     
 
-def evaluaCodigo():
+def evaluaCodigo():                         #comprueba tipo y resultado
+    resultadoBool = ((type(resultCode) is int) and (resultCode == 4))
+    return resultadoBool
+
+
+def button_click(event):                    #crear antes de create_proxy
+    execCodigo()
+    resultadoBool = evaluaCodigo()
     print('resultado a comprobar',resultCode)
-    if condicionesNecesarias(resultCode):              
+    if resultadoBool:              
         print('Resultado correcto')         #print devulelve el salto de linea por defecto 
     else:
         print('Resultado incorrecto')
-
-def condicionesNecesarias(resultCode):      #comprobamos tipo y resultado
-    return ((type(resultCode) is int) and (resultCode == 4))
-
-def button_click(event):                    #crear antes de create_proxy
- execCodigo()
- evaluaCodigo()
 
 click_proxy = create_proxy(button_click)
 #document.getElementById("buttonRun").addEventListener("click", click_proxy)
