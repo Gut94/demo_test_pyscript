7fd739207215628c67a6c83764bc77f85b3e3490
Gut_PC <raulnlh@gmail.com>
2022-12-03 13:57:28 +0100

    Mensajes de error y modificador pyscript, error al imprimir por html y consola

--- python/controlador.py
+++ python/controlador.py
@@ -16,36 +16,54 @@ def execCodigo():
     exec(stringCodeMod)
     resultCode = x                          #variable a sacar del codigo pasado a exec
     print('x',resultCode)
-    imprimePorHTML()
+    
+
 
 def evaluaCodigo():                         #comprueba tipo y resultado
-    resultadoBool = ((type(resultCode) is int) and (resultCode == 4))
+    resultadoBool = condicion1() and condicion2()
     return resultadoBool
+
+def condicion1():                           #comprueba tipo
+    condicion1Bool = (type(resultCode) is int)
+    return condicion1Bool
+
+def condicion2():                           #comprueba resultado
+    condicion2Bool = (resultCode == 4)
+    return condicion2Bool
+
+
 
 def imprimePorHTML():
     print('hola')
     resultadoTextArea1 = document.getElementById("resultadoTextarea1")
-    time.sleep(1)
     #resultadoTextArea1.select()
     resultadoTextArea1.innerHTML = resultCode
-    time.sleep(1)
+    condicionesBool = evaluaCodigo()
+    print('resultado a comprobar',resultCode)
+    if condicionesBool:              
+        print('Resultado correcto')         #print devulelve el salto de linea por defecto 
+        document.getElementById("resultadoTextarea1").style.backgroundColor = "#90EE90"     #green
+        document.getElementById("alertas").style.display = 'flex'
+        document.getElementById("alertaCorrecto").style.display = 'block'
+        document.getElementById("alertaError").style.display = 'none'
+    else:
+        print('Resultado incorrecto')
+        document.getElementById("resultadoTextarea1").style.backgroundColor = "#ffcccb"     #red
+        document.getElementById("alertas").style.display = 'flex'
+        document.getElementById("alertaCorrecto").style.display = 'none'
+        document.getElementById("alertaError").style.display = 'block'
+
+
 
 def getstrCode():
     return document.getElementById("codeTextarea1").value
 
-def button_click(event):                    #crear antes de create_proxy
-    time.sleep(1)
-    execCodigo()
-    resultadoBool = evaluaCodigo()
-    print('resultado a comprobar',resultCode)
-    if resultadoBool:              
-        print('Resultado correcto')         #print devulelve el salto de linea por defecto 
-        document.getElementById("resultadoTextarea1").style.backgroundColor = "#90EE90"     #green
-    else:
-        print('Resultado incorrecto')
-        document.getElementById("resultadoTextarea1").style.backgroundColor = "#ffcccb"     #red
 
+def button_click(event):                    #crear antes de create_proxy
+    execCodigo()
+    imprimePorHTML()
     
+
 
 click_proxy = create_proxy(button_click)
 #document.getElementById("buttonRun").addEventListener("click", click_proxy)
